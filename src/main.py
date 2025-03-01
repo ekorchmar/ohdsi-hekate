0fe1c13643a73b096a595a68e43749bd5e582154
Eduard Korchmar <korchmar@ohdsi.org>
2025-03-01 13:29:24 +0100

    test: document a bug happening to an implicitly invalid concept

--- src/main.py
+++ src/main.py
@@ -17,10 +17,31 @@ if __name__ == '__main__':
     path = Path("~/Downloads/Vocab/").expanduser()
     LOGGER.info(f"Starting processing of Athena Vocabularies from {path}")
     a = OMOPVocabulariesV5(vocab_download_path=path)
-    counts = {k: len(v) for k, v in a.atoms.precise_ingredient.items()}
-    from collections import Counter
-    import json
 
-    c = Counter(counts.values())
-    print(json.dumps(c, indent=2))
+    from typing import override
+    import rustworkx as rx
+    from rx_model.drug_classes import ConceptId, ClinicalDrugForm
+
+    # BUG: 43147050 has 8 valid "Ingredient" relations, one of which is
+    # actually a "Precise Ingredient", and another is invalid.
+    # Current behavior is to "accept" it as a CDC with 6(not 8) ingredients,
+    # but we may want to raise an error instead.
+
+    class IFind43147050(rx.visit.DFSVisitor):
+
+        @override
+        def discover_vertex(self, v: int, t: int):
+            del t  # Discover time
+            node: ClinicalDrugForm[ConceptId] = a.hierarchy.graph[v]
+            if node.identifier == ConceptId(43147050):
+                for ing in node.ingredients:
+                    print(ing)
+                raise ValueError("Found it!")
+
+    rx.dfs_search(
+        a.hierarchy.graph,
+        list(a.hierarchy.ingredients.values()),
+        IFind43147050()
+    )
+
     print("Done")
