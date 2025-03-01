72d6a0697d53e0e551e41cd0e7b0691ab3466020
Eduard Korchmar <korchmar@ohdsi.org>
2025-03-01 10:33:33 +0100

    docs: more queries for CDC research

---
+++ reference/source_errors/cdc_specifying_pi.sql
@@ -0,0 +1,24 @@
+select
+    c1.concept_id,
+    c1.concept_name,
+    r.relationship_id,
+    c2.concept_id as ingredient_concept_id,
+    c2.concept_name as ingredient_concept_name,
+    c2.concept_class_id as ingredient_concept_class_id
+from concept c1
+join
+    concept_relationship r
+    on c1.concept_id = r.concept_id_1
+    and c1.vocabulary_id in ('RxNorm', 'RxNorm Extension')
+    and r.invalid_reason is null
+    and c1.invalid_reason is null
+join
+    concept c2
+    on c2.vocabulary_id in ('RxNorm', 'RxNorm Extension')
+    and c2.invalid_reason is null
+    and c2.concept_id = r.concept_id_2
+where
+    c1.concept_class_id = 'Clinical Drug Form'
+    and c2.concept_class_id = 'Precise Ingredient'
+    and r.relationship_id = 'RxNorm has ing'
+;
