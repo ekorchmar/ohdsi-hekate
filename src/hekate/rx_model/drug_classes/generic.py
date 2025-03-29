7245709423214bfd172a1f104cee9f60b0677fbf
Eduard Korchmar <korchmar@ohdsi.org>
2025-03-29 16:06:49 +0100

    feat: add generic alternative constructor for use with declarative interface

--- src/hekate/rx_model/drug_classes/generic.py
+++ src/hekate/rx_model/drug_classes/generic.py
@@ -2,6 +2,8 @@
 Contains generic data classes and types used throughout the project.
 """
 
+from __future__ import annotations
+
 from abc import ABC, abstractmethod  # For DrugNode interface
 from typing import (
     TYPE_CHECKING,  # For conditional imports for typechecking
@@ -17,6 +19,10 @@ if TYPE_CHECKING:
 
     # Circular import: Strength needs ConceptId
     import rx_model.drug_classes.strength as st
+
+    from rx_model.descriptive.base import (
+        ConceptClassId,  # For alternative constructor
+    )
 
 
 # Identifiers
@@ -43,12 +49,12 @@ class ConceptCodeVocab(NamedTuple):
 
 type ConceptIdentifier = ConceptId | ConceptCodeVocab
 
-type BoundStrength[Id: ConceptIdentifier, S: "st.Strength | None"] = tuple[
+type BoundStrength[Id: ConceptIdentifier, S: st.Strength | None] = tuple[
     "a.Ingredient[Id]", S
 ]
 
 
-class DrugNode[Id: ConceptIdentifier, S: "st.Strength | None"](ABC):
+class DrugNode[Id: ConceptIdentifier, S: st.Strength | None](ABC):
     """
     Metaclass for the nodes in the drug concept hierarchy.
 
@@ -61,7 +67,7 @@ class DrugNode[Id: ConceptIdentifier, S: "st.Strength | None"](ABC):
     @abstractmethod
     def is_superclass_of(
         self,
-        other: "DrugNode[Id, st.Strength | None]",
+        other: DrugNode[Id, st.Strength | None],
         passed_hierarchy_checks: bool = True,
     ) -> bool:
         """
@@ -85,7 +91,7 @@ class DrugNode[Id: ConceptIdentifier, S: "st.Strength | None"](ABC):
         """
 
     @abstractmethod
-    def get_precise_ingredients(self) -> list["a.PreciseIngredient | None"]:
+    def get_precise_ingredients(self) -> list[a.PreciseIngredient | None]:
         """
         Retrieve all Precise a.Ingredients participating in this node. Data
         will be returned as a sequence of Precise Ingredient instances or None,
@@ -94,19 +100,19 @@ class DrugNode[Id: ConceptIdentifier, S: "st.Strength | None"](ABC):
 
     # Methods to get a possibly inherited attribute
     # NOTE: These methods default to returning None. Be careful in subclasses!
-    def get_brand_name(self) -> "a.BrandName[Id] | None":
+    def get_brand_name(self) -> a.BrandName[Id] | None:
         """
         Retrieve the brand name for this node.
         """
         return None
 
-    def get_dose_form(self) -> "a.DoseForm[Id] | None":
+    def get_dose_form(self) -> a.DoseForm[Id] | None:
         """
         Retrieve the dose form for this node.
         """
         return None
 
-    def get_supplier(self) -> "a.Supplier[Id] | None":
+    def get_supplier(self) -> a.Supplier[Id] | None:
         """
         Retrieve the a.supplier for this node.
         """
@@ -117,3 +123,26 @@ class DrugNode[Id: ConceptIdentifier, S: "st.Strength | None"](ABC):
         Retrieve the box size for this node.
         """
         return None
+
+    @classmethod
+    @abstractmethod
+    def from_definitions(
+        cls,
+        identifier: Id,
+        parents: dict[ConceptClassId, list[DrugNode[Id, st.Strength | None]]],
+        attributes: dict[
+            ConceptClassId, a.BrandName[Id] | a.DoseForm[Id] | a.Supplier[Id]
+        ],
+        precise_ingredients: list[a.PreciseIngredient],
+        strength_data: SortedTuple[BoundStrength[Id, S]],
+        box_size: int | None,
+    ) -> DrugNode[Id, S]:
+        """
+        Create a DrugNode instance from concept definitions.
+
+        Note that definitions are allowed to be redundant, so each class
+        implments its own logic to pick and choose the correct data.
+        """
+        raise NotImplementedError(
+            f"{cls.__name__} should not be constructed from definitions."
+        )
