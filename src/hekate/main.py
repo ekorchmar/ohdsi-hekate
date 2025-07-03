4289175f8534ebcbe0f3560e330ab54e7d9c5977
Eduard Korchmar <korchmar@ohdsi.org>
2025-07-03 21:58:42 +0200

    chore: rework run set-up

--- src/hekate/main.py
+++ src/hekate/main.py
@@ -10,7 +10,9 @@ def _main():
 if __name__ == "__main__":
     import sys
 
-    sys.argv = (
-        "hekate -a $ATHENA_DOWNLOAD_DIR -b $BUILD_RXE_DOWNLOAD_DIR".split()
-    )
+    sys.argv = "hekate \
+        -a $ATHENA_DOWNLOAD_DIR \
+        -b $BUILD_RXE_DOWNLOAD_DIR \
+        -g GGR:3551140 \
+        ".split()
     _main()
